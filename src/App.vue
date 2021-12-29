f8fbd6a6c29e74ac112d1eea8186b2fe6ed727f0
diegofreitas1 <diegofreitas13@gmail.com>
2021-12-29 10:04:24 -0300

    finalizando

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app> 
+    <v-app-bar app color="#546E7A" dark>
+      <v-app-bar-title> Vue </v-app-bar-title>
+      <v-spacer></v-spacer>
+            <v-btn
+        v-for="(link, i) in links"
+        :key="i"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        :to="link.path"
+      >
+        {{ link.label }}
+      </v-btn>
+    </v-app-bar>
+    <!-- Login -->
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  <v-footer
+    color="#546E7A"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="(link, i) in links"
+        :key="i"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        :to="link.path"
+      >
+        {{ link.label }}
+      </v-btn>
+      <v-col
+        class="blue-grey lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {},
+
+  data: () => ({
+    links: [{
+      label: "Home",
+      path: "/",
+    },
+    {
+      label: "Login",
+      path: "login",
+    },
+    {
+      label: "About",
+      path: "about",
+    },
+    ],
+  }),
+  methods: {},
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import VueTheMask from 'vue-the-mask'
 
 Vue.config.productionTip = false
+Vue.use(VueTheMask)
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
